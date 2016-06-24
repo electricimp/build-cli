344df4e90cd47a08a8e9de513d89aed8f829355c
Electric Imp, Inc <tone_s@blueyonder.co.uk>
2016-06-24 16:02:56 +0100

    Tidy code

--- bin/imp-push.js
+++ bin/imp-push.js
@@ -7,9 +7,6 @@ var fs = require("fs");
 var ImpConfig = require("../lib/impConfig.js");
 var config = new ImpConfig();
 
-//program
-//  .option("-t, --tag [tag]", "adds a tag to the revision");
-
 program.parse(process.argv);
 
 config.init(["apiKey", "modelId", "agentFile", "deviceFile",], function(err, success) {
@@ -31,9 +28,6 @@ config.init(["apiKey", "modelId", "agentFile", "deviceFile",], function(err, suc
   model.agent_code = fs.readFileSync(config.get("agentFile"), "utf8");
   model.device_code = fs.readFileSync(config.get("deviceFile"), "utf8");
 
-  // Add the tag (if one was specified)
-  // if ("tag" in program) model["marker"] = program.tag;
-
   imp = config.createImpWithConfig();
   imp.createModelRevision(config.get("modelId"), model, function(err, data) {
     if (err) {
@@ -43,18 +37,20 @@ config.init(["apiKey", "modelId", "agentFile", "deviceFile",], function(err, suc
       }
 
       if (err.details.agent_errors) {
-        for(var i = 0; i < err.details.agent_errors.length; i ++) {
+        // List out agent code compilation errors (returned by server)
+        for(var i = 0; i < err.details.agent_errors.length; i++) {
           var thisErr = err.details.agent_errors[i];
           console.log(colors.red("ERROR: " + thisErr.error));
-          console.log("   at: " + config.get("agentFile") +":" + thisErr.row + " (col "+thisErr.column+")");
+          console.log("   at: " + config.get("agentFile") + ":" + thisErr.row + " (col " + thisErr.column + ")");
         }
       }
 
       if (err.details.device_errors) {
-        for(var i = 0; i < err.details.device_errors.length; i ++) {
+        // List out agent code compilation errors (returned by server)
+        for(var i = 0; i < err.details.device_errors.length; i++) {
           var thisErr = err.details.device_errors[i];
           console.log(colors.red("ERROR: " + thisErr.error));
-          console.log("   at: " + config.get("deviceFile") +":" + thisErr.row + " (col "+thisErr.column+")");
+          console.log("   at: " + config.get("deviceFile") + ":" + thisErr.row + " (col " + thisErr.column + ")");
         }
       }
 
@@ -69,5 +65,4 @@ config.init(["apiKey", "modelId", "agentFile", "deviceFile",], function(err, suc
       console.log("Successfully created revision " + data.revision.version);
     });
   });
-
 });
