fb8c48166000085d8a6447dd125f357608e46802
Electric Imp, Inc <tone_s@blueyonder.co.uk>
2016-06-24 16:14:24 +0100

    login -> setup, init -> new

--- bin/imp.js
+++ bin/imp.js
@@ -6,13 +6,13 @@ var pkg = require("../package.json");
 program
   .version(pkg.version)
 
-  .command("devices [options]", "list and manage devices")
-  .command("init", "create an empty imp project, or reinitialize an existing one")
-  .command("log [options]", "display logs from a specified device")
-  .command("login [options]", "sets your global API-Key")
-  .command("migrate [options]", "migrates a model from one account to another")
-  .command("models [options]", "list and manage models")
-  .command("pull [options]", "fetch latest build from the server")
-  .command("push [options]", "update the build, and push code to developer devices")
+  .command("devices [options]", "List and manage devices")
+  .command("new", "Create a new imp project, or reinitialize an existing one")
+  .command("log [options]", "Display logs from a specified device")
+  .command("setup [options]", "Sets your global Build API Key")
+  .command("migrate [options]", "Migrates a model from one account to another")
+  .command("models [options]", "List and manage models")
+  .command("pull [options]", "Fetch latest build from the server")
+  .command("push [options]", "Update the build, and push code to developer devices")
 
 program.parse(process.argv);
