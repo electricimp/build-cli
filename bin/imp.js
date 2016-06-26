a28a66ef41b1489a20b9ba44b5b76b33a8a6a059
Electric Imp, Inc <tone_s@blueyonder.co.uk>
2016-06-26 11:24:57 +0100

    Update imp.js
    Re-instate 'imp init'

--- bin/imp.js
+++ bin/imp.js
@@ -4,15 +4,16 @@ var program = require("commander");
 var pkg = require("../package.json");
 
 program
-  .version(pkg.version)
+    .version(pkg.version)
 
-  .command("devices [options]", "List and manage devices")
-  .command("new", "Create a new imp project, or reinitialize an existing one")
-  .command("log [options]", "Display logs from a specified device")
-  .command("setup [options]", "Sets your global Build API Key")
-  .command("migrate [options]", "Migrates a model from one account to another")
-  .command("models [options]", "List and manage models")
-  .command("pull [options]", "Fetch latest build from the server")
-  .command("push [options]", "Update the build, and push code to developer devices")
+    .command("setup [options]", "Sets your global Build API Key")
+    .command("new", "Create a new imp project, or reinitialize an existing one")
+    .command("init", "Create a new imp project, or reinitialize an existing one")
+    .command("devices [options]", "List and manage devices")
+    .command("models [options]", "List and manage models")
+    .command("log [options]", "Display logs from a specified device")
+    .command("pull [options]", "Fetch latest build from the server")
+    .command("push [options]", "Update the build, and push code to developer devices")
+    .command("migrate [options]", "Migrates a model from one account to another")
 
 program.parse(process.argv);
