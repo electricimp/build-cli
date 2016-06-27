13740b67f635cb6ec2f722a1387ec5e654b10ced
Electric Imp, Inc <tone_s@blueyonder.co.uk>
2016-06-27 13:29:55 +0100

    Update imp-pull.js

--- bin/imp-pull.js
+++ bin/imp-pull.js
@@ -11,71 +11,73 @@ var config = new ImpConfig();
 var imp;
 
 program
-  .option("-r, --revision [revision]", "pulls the specified revision")
-  .option("-d, --devices", "pulls and syncs device list")
+    .option("-r, --revision <revision>", "Pulls the specified revision")
+    .option("-d, --devices", "Pulls and syncs the model's list of assigned devices")
 
 program.parse(process.argv);
 
 function getVersion(ver, cb) {
-  imp.getModelRevision(config.get("modelId"), ver, cb);
+    imp.getModelRevision(config.get("modelId"), ver, cb);
 }
 
 function done(err, data) {
-  if (err) {
-    console.log("ERROR: " + err.message_short);
-    return;
-  }
-  else {
-    if ("revision" in data) {
-      fs.writeFile(config.get("deviceFile"), data.revision.device_code);
-      fs.writeFile(config.get("agentFile"), data.revision.agent_code);
-
-      console.log("Update local code to version " + data.revision.version);
+    if (err) {
+        console.log("ERROR: " + err.message_short);
+        return;
+    } else {
+        if ("revision" in data) {
+            fs.writeFile(config.get("deviceFile"), data.revision.device_code);
+            fs.writeFile(config.get("agentFile"), data.revision.agent_code);
+            console.log("Updated local code to version " + data.revision.version);
+        }
     }
-  }
 }
 
 config.init(["apiKey", "modelId", "devices", "agentFile", "deviceFile"], function(err, success) {
-  if (err) {
-    console.log("ERROR: " + err);
-    return;
-  }
-
-  imp = config.createImpWithConfig();
-
-  if ("devices" in program) {
-    imp.getDevices( { "model_id": config.get("modelId") }, function(err, data) {
-      if (err) {
+    if (err) {
         console.log("ERROR: " + err);
         return;
-      } else {
-        var devices = [];
-        data.devices.forEach(function(device) {
-          devices.push(device.id);
-        });
-        config.setLocal("devices", devices);
-        config.saveLocalConfig(function(err, success) {
-          if (err) {
-            console.log("ERROR: " + err);
-            return;
-          }
-          console.log("Update local devices list for model " + config.get("modelId"));
-        });
-      }
-    });
-  } else {
-    if ("revision" in program) {
-      getVersion(program.revision, done);
-    } else {
-      imp.getModelRevisions(config.get("modelId"), null, function(err, data) {
-        if (err) {
-          console.log("ERROR: " + err);
-          return;
-        }
-        if ("revisions" in data && data.revisions.length > 0) {
-          getVersion(data.revisions[0].version, done);
-        }
-      });
     }
-  }
+
+    imp = config.createImpWithConfig();
+
+    if ("devices" in program) {
+        imp.getDevices({ "model_id": config.get("modelId") }, function(err, data) {
+            if (err) {
+                console.log("ERROR: " + err);
+                return;
+            } else {
+                var devices = [];
+                data.devices.forEach(function(device) {
+                    devices.push(device.id);
+                });
+
+                config.setLocal("devices", devices);
+                config.saveLocalConfig(function(err, success) {
+                    if (err) {
+                        console.log("ERROR: " + err);
+                        return;
+                    }
+
+					var modelName = config.getLocal("modelName");
+                    console.log("Updated list of devices for model '" + modelName + "'");
+                });
+            }
+        });
+    } else {
+        if ("revision" in program) {
+            getVersion(program.revision, done);
+        } else {
+            imp.getModelRevisions(config.get("modelId"), null, function(err, data) {
+                if (err) {
+                    console.log("ERROR: " + err);
+                    return;
+                }
+
+                if ("revisions" in data && data.revisions.length > 0) {
+                    getVersion(data.revisions[0].version, done);
+                }
+            });
+        }
+    }
 });
