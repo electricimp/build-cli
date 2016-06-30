d00faa2944769184d023fb40f47264f3d71ff4e0
Electric Imp, Inc <ElectricImpSampleCode@users.noreply.github.com>
2016-06-30 10:39:09 +0100

    Update imp-models.js

--- bin/imp-models.js
+++ bin/imp-models.js
@@ -12,27 +12,27 @@ var ImpConfig = require("../lib/impConfig.js");
 var config = new ImpConfig();
 
 program
-    .option("--active", "Filters list to only display active models")
-    .option("--inactive", "Filters list to only display inactive models")
+    .option("-a, --active", "Lists only active models (those with assigned devices)")
+    .option("-i, --inactive", "Lists only inactive models (those with no assigned devices)")
     .option("-d, --device <deviceID/deviceName>", "Lists the model to which the device is assigned (if any)")
 
 program.parse(process.argv);
 
 config.init(["apiKey"], function(err, success) {
     if (err) {
-        console.log("ERROR: Global Build API key is not set - run 'imp setup' then try 'imp models' again");
+        console.log("ERROR: Global Build API key is not set. Run 'imp setup' then try 'imp models' again");
         return;
     }
 
     imp = config.createImpWithConfig();
 
     if ("active" in program && "inactive" in program) {
-        console.log("ERROR: You cannot specify --active and --inactive");
+        console.log("ERROR: You cannot specify the options --active and --inactive");
         return;
     }
 
     if ("inactive" in program && "device" in program) {
-        console.log("ERROR: You cannot specify --device and --inactive");
+        console.log("ERROR: You cannot specify the options --device and --inactive");
         return;
     }
 
@@ -56,9 +56,9 @@ config.init(["apiKey"], function(err, success) {
             var filteredModels = [];
             modelData.models.some(function(model) {
                 if (activeState == null) {
-                    // Not filtering by state, so just push it to the list
+                    // Not filtering models by state (active/inactive)
                     if ("device" in program) {
-                        // We have a device specified is it an ID?
+                        // We have a device specified - is it specified by an ID?
                         deviceData.devices.some(function(device) {
                             if (device.id == program.device) {
                                 if (device.model_id == model.id) filteredModels.push(model);
@@ -66,8 +66,8 @@ config.init(["apiKey"], function(err, success) {
                             }
                         });
 
-                        // An ID has been passed, but it didn't match,
-                        // so was it a device name? Match on lower case
+                        // The passed device specifier didn't match any device ID,
+                        // so perhaps was it a device name? Match on lower case
                         deviceData.devices.some(function(device) {
                             if (device.name.toLowerCase() == program.device.toLowerCase()) {
                                 if (device.model_id == model.id) filteredModels.push(model);
@@ -75,11 +75,11 @@ config.init(["apiKey"], function(err, success) {
                             }
                         });
                     } else {
+                        // Listing all models so just push this model to the list
                         filteredModels.push(model);
                     }
                 } else if (activeState) {
-                    // Filtering by state == active, so check for
-                    // device associations
+                    // Filtering by state == active, so check for device associations
                     deviceData.devices.some(function(device) {
                         if (device.model_id == model.id) {
                             // At least one device is listing this as its model,
@@ -98,20 +98,20 @@ config.init(["apiKey"], function(err, success) {
                         }
                     });
 
-                    // Model is not associated with any device, ie. it's inactive
-                    // so push it to the list
+                    // Model is not associated with any device (found == false)
+                    // ie. it's inactive, so push it to the list
                     if (!found) filteredModels.push(model);
                 }
             });
 
             if (filteredModels.length > 0) {
+                // We have model(s) to display
                 if ("device" in program) {
-                    // A device can only have one model, so list that
+                    // A device can only have one model, so display that
                     console.log("Device '" + program.device + "' is assigned to model '" + filteredModels[0].name + "' (ID: " + filteredModels[0].id + ")");
                     return;
                 }
 
-                // We have models to list, so create the list as a table
                 var header = ['Model Name', 'Model ID'];
                 for (var index in header) {
                     // Set the header colour
